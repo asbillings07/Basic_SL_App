212fb4803b90213cbeccd2ccdd3f59c7edc7bb03
Aaron Billings <asbillings2007@gmail.com>
2019-08-16 21:03:28 -0400

    lifted state to App componenet

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import People from './Components/People';
-import Navigator from './Components/Navigator'
+import Navigator from './Components/Navigator';
+import axios from 'axios';
+import Config from './Config';
+import Frequency from './Components/Frequency';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-const App = () => (
-  <>
-    <Navigator />
-    <People />
-  </>
-);
+export default class App extends Component {
+  state = {
+    people: [],
+    email: [],
+  };
 
-export default App;
+  componentDidMount() {
+    this.goGetPeople();
+  }
+
+  goGetPeople = async () => {
+    const res = await axios.get(`${Config.apiBaseURL}`);
+    const people = res.data.data;
+    this.setState({
+      people,
+      email: people.map(person => person.email_address),
+    });
+    console.log(this.state.people);
+  };
+
+  render() {
+    return (
+      <Router>
+        <Navigator />
+        <Swtich>
+          <Route
+            exact
+            path="/"
+            component={() => <People people={this.state.people} />}
+          />
+          <Router
+            path="/frequency"
+            component={() => <Frequency email={this.state.email} />}
+          />
+        </Swtich>
+      </Router>
+    );
+  }
+}
--- client/src/Components/People.js
+++ client/src/Components/People.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Config from '../Config'
-import { Container, Row, Col, Card, CardGroup } from 'react-bootstrap'
-import styled from 'styled-components'
+import Config from '../Config';
+import { Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
+import styled from 'styled-components';
 
-
-
-export default class People extends Component {
-  state = {
-    people: [],
-    email: [],
-  };
-
-  componentDidMount() {
-    this.goGetPeople();
-  }
-
-  goGetPeople = async () => {
-
-    const res = await axios.get(`${Config.apiBaseURL}`)
-    const people = res.data.data
-    this.setState({
-      people,
-      email: people.map(person => person.email_address)
-    })
-    console.log(this.state.people)
-  };
-
-  showPeople = () => {
-
-    const { people } = this.state
-
+const People = ({ people }) => {
+  const showPeople = () => {
     return people.map(person => (
       <Col sm={4} key={person.id}>
         <StyledCard>
@@ -41,34 +16,30 @@ export default class People extends Component {
             <Card.Text>city: {person.city}</Card.Text>
             <Card.Text>state: {person.state}</Card.Text>
             <Card.Text>phone: {person.phone}</Card.Text>
-            <Card.Text>company website: {person.person_company_website}</Card.Text>
-
+            <Card.Text>
+              company website: {person.person_company_website}
+            </Card.Text>
           </Card.Body>
         </StyledCard>
       </Col>
-    ))
+    ));
+  };
 
-  }
+  return (
+    <Container>
+      <Row>
+        <CardGroup>{showPeople()}</CardGroup>
+      </Row>
+    </Container>
+  );
+};
 
-
-  render() {
-    return (
-      <Container>
-        <Row>
-          <CardGroup>
-            {this.showPeople()}
-          </CardGroup>
-        </Row>
-      </Container>
-    );
-  }
-}
+export default People;
 
 const StyledCard = styled(Card)`
-width: '22rem';
-margin-top: 3px;
-&:hover {
-  background:#53a7ea;
-}
-
-`
+  width: '22rem';
+  margin-top: 3px;
+  &:hover {
+    background: #53a7ea;
+  }
+`;
