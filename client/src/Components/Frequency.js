45e35072816a3b2efd83666bebba93640a46cff6
Aaron Billings <asbillings2007@gmail.com>
2019-08-17 13:33:00 -0400

    add frequency of letter function

--- client/src/Components/Frequency.js
+++ client/src/Components/Frequency.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import Table from 'react-bootstrap/Table';
 
-const Frequency = () => {
+const Frequency = ({ email }) => {
+  // grabs the email via props and gets all the chars from the emails
+  const str = email.join('').split('');
 
-}
+  /**Function @param(String) */
+  const frequencyOfLetters = letters => {
+    let mapObj = {};
 
-export default Frequency
+    letters.map(letter => {
+      if (mapObj[letter]) {
+        mapObj[letter].push(letter);
+      } else {
+        mapObj[letter] = [letter];
+      }
+    });
+
+    return Object.entries(mapObj).map((keyName, i) => (
+      <tbody key={i}>
+        <tr>
+          <td>{keyName[0]}</td>
+          {console.log(keyName)}
+          <td>{keyName[1].length}</td>
+        </tr>
+      </tbody>
+    ));
+  };
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Letter</th>
+          <th>Frequency</th>
+        </tr>
+      </thead>
+      {frequencyOfLetters(str)}
+    </Table>
+  );
+};
+
+export default Frequency;
